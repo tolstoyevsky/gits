bfffc068045ef1be38400ea68eccb580de984668
Dmitriy Shilin <sdadeveloper@gmail.com>
2016-12-18 11:21:45 +0300

    Tests: Refactoring
    * Add `_check_cap_kcuu1`, `_check_cap_kcud1` helper functions.
    * Rework `test_cap_kcuu1`, `test_cap_kcud1`.

--- test_emulator.py
+++ test_emulator.py
@@ -507,7 +507,6 @@ class TestEmulator(unittest.TestCase):
         self._terminal.esc_ri('')
         self.assertEqual(rand_y - 1, self._terminal._cur_y)
 
-
     @unittest.skip("skip")
     def test_cap_set_colour_pair(self):
         pass
@@ -603,11 +602,11 @@ class TestEmulator(unittest.TestCase):
         self.assertEqual(cur_x_bck, term._cur_x)
         self.assertTrue(term._eol)
 
-    @unittest.skip("skip")
+    @unittest.skip('skip')
     def test_cap_ich1(self):
         # ATTN: corresponding method has the following doc: Insert character.
-        #       Is it ok? I'm confused because this method just executes scroll
-        #       right terminal method.
+        #       Is it ok? I'm confused because this method just executes
+        #       `scroll_right` terminal method.
 
         """The terminal should have the possibility to insert character. """
         pass
@@ -618,11 +617,73 @@ class TestEmulator(unittest.TestCase):
         self._terminal.cap_smso()
         self.assertEqual(0x70000000, self._terminal._sgr)
 
-    @unittest.skip("skip")
-    def test_cap_kcuu1(self):
-        pass
+    def _check_cap_kcuu1(self, l, pos, want_cur_y):
+        """A helper function that checks `cap_kcuu1` method.
 
-    @unittest.skip("skip")
+        The ``l`` argument ... . (Need Eugene comments)
+        The ``pos`` argument must be a tuple or list of coordinates ``(x, y)``.
+        The ``want_cur_y`` argument is an expected terminal's `cur_y` value
+        after `cap_kcud1` method.
+        """
+
+        self._terminal._cur_x, self._terminal._cur_y = pos
+        self._terminal.cap_kcuu1(l)
+        self.assertEqual(want_cur_y, self._terminal._cur_y)
+
+        self._terminal.cap_rs1()
+
+    def test_cap_kcuu1(self):
+        """The terminal should have the possibility to receive string of input
+        characters sent by typing the up-arrow key.
+        ATTN: rework after Eugene comments.
+        """
+
+        term = self._terminal
+
+        # Terminal's `cur_y` is on the top-most position.
+        self._check_cap_kcuu1([1], (0, 0), term._top)
+
+        # Terminal's `cur_y` is on the bottom-most position.
+        l = [1]
+        self._check_cap_kcuu1(l, (0, term._bottom), term._bottom - l[0])
+
+        # Terminal's `cur_y` is on the random position.
+        rand_y = random.randint(1, term._bottom - 1)
+        l = [1]
+        self._check_cap_kcuu1(l, (0, rand_y), rand_y - l[0])
+
+    def _check_cap_kcud1(self, l, pos, want_cur_y):
+        """A helper function that checks `cap_kcud1` method.
+
+        The ``l`` argument ... . (Need Eugene comments)
+        The ``pos`` argument must be a tuple or list of coordinates ``(x, y)``.
+        The ``want_cur_y`` argument is an expected terminal's `cur_y` value
+        after `cap_kcud1` method.
+        """
+
+        self._terminal._cur_x, self._terminal._cur_y = pos
+        self._terminal.cap_kcud1(l)
+        self.assertEqual(want_cur_y, self._terminal._cur_y)
+
+        self._terminal.cap_rs1()
+
+    def test_cap_kcud1(self):
+        # ATTN: rework after Eugene comments.
+
+        term = self._terminal
+
+        # Terminal's `cur_y` is on the top-most position.
+        self._check_cap_kcud1([1], (0, 0), 1)
+
+        # Terminal's `cur_y` is on the bottom-most position.
+        self._check_cap_kcud1([1], (0, term._bottom), term._bottom)
+
+        # Terminal's `cur_y` is on the random position.
+        rand_y = random.randint(1, term._bottom - 1)
+        l = [1]
+        self._check_cap_kcud1(l, (0, rand_y), rand_y + l[0])
+
+    @unittest.skip('skip')
     def test_cap_kcuf1(self):
         pass
 
