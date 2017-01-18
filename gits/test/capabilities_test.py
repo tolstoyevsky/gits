ace4becfe95902d9cbce8cf6ddc8af6ae50334eb
Dmitriy Shilin <sdadeveloper@gmail.com>
2017-01-18 08:47:36 +0300

    Add test for `op` capability

--- gits/test/capabilities_test.py
+++ gits/test/capabilities_test.py
@@ -532,6 +532,27 @@ class TestCapabilities(Helper):
         rand_y = random.randint(1, term._bottom_most - 1)
         self._check_cap_kcuu1((0, rand_y), rand_y - 1)
 
+    def test_cap_op(self):
+        """The terminal should have the possibility to set default color-pair
+        to the original one.
+        """
+        self._terminal._cap_op()
+        self.assertEqual(MAGIC_NUMBER, self._terminal._sgr)
+
+    def test_cap_ri(self):
+        """The terminal should have the possibility to scroll text down. """
+        term = self._terminal
+
+        # Put the text on the first line.
+        self._check_cap_ri(['x'] * term._right_most, (0, 0))
+
+        # Put the text on the second line.
+        self._check_cap_ri(['x'] * term._right_most, (0, 1))
+
+        # Put the text on an arbitrary line.
+        rand_y = random.randint(2, term._bottom_most)
+        self._check_cap_ri(['x'] * term._right_most, (0, rand_y))
+
     def test_cap_rc(self):
         """The terminal should have the possibility to restore the cursor to
         the last saved position.
